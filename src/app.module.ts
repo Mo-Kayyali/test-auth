ed2cf8b5487ea32b1623e1ffea8aeea46720c526
Kayyali <mohamedelkayyali2@gmail.com>
2026-03-01 03:25:30 +0200

    false3

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { ConfigModule } from '@nestjs/config';
       password: 'pass123',
       database: 'postgres',
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
     }),
     IamModule,
   ],
